e71f99bc618cd6b1978e2c06300ce326ca026290
Sergio Castillo <s.cast.lara@gmail.com>
2015-10-14 21:31:06 +0200

    Few changes. Nothing important.

--- ALG/cross_implementation.pl
+++ ALG/cross_implementation.pl
@@ -3,17 +3,21 @@ use warnings;
 use strict;
 use GD::Simple;
 
-
-my $nmax = shift @ARGV;
-my $l    = 50;
+#-- VARIABLES --#
+my $nmax  = shift @ARGV;
+my $l     = 50;
 my $s_dir = [[0,1], [1,0], [-1, 0], [0,-1]];
 my $o_dir = [[1,1], [-1,-1], [-1, 1], [1,-1]];
 my $init  = [[200,125]];
+my $img   = GD::Simple->new(400,250);
 my $n = 0;
-my $img = GD::Simple->new(400,250);
+
+#-- MAIN --#
 cross($n, $nmax, $init, $s_dir, $o_dir, $l, $img);
 print $img->png;
 
+
+#-- FUNCTIONS --#
 sub cross {
 	my ($n, $nmax, $init, $s_dir, $o_dir, $l, $img) = @_;
 	
