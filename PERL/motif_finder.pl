4eb7985f29c09d0eddc124803e396af35a92be36
Sergio Castillo <s.cast.lara@gmail.com>
2015-10-21 21:23:00 +0200

    Motif now in separated variable.

--- PERL/motif_finder.pl
+++ PERL/motif_finder.pl
@@ -3,11 +3,12 @@ use strict;
 
 local $/ = ">"; 
 <>; 
+my $motif = "C.{2,4}C.{3}[LIVMFYWC].{8}H.{3,5}H";
 
 while(<>){
 	chomp; 
 	my ($id, @seq) = split /\n/; 
 	my $seq = join("", @seq); 
 	print ">$id\n$seq\n" 
-		if ($seq =~ s/(C.{2,4}C.{3}[LIVMFYWC].{8}H.{3,5}H)/\-\[$1\]\-/gi);
+		if ($seq =~ s/($motif)/\-\[$1\]\-/gi);
 }
