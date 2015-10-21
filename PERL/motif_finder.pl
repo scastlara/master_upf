0d1db9284cda68f2fca4e450c5a40c35985f0872
Sergio Castillo <s.cast.lara@gmail.com>
2015-10-21 21:46:03 +0200

    Comm

--- PERL/motif_finder.pl
+++ PERL/motif_finder.pl
@@ -3,7 +3,8 @@ use strict;
 
 local $/ = ">"; # This magic variable changes the record separator.
                 # It will make the diamond operator read from > to >
-                # instead of from \n to \n (line by line)
+                # By default it is set to "\n", that's why <> reads "line" by "line".
+                
 my $motif = "C.{2,4}C.{3}[LIVMFYWC].{8}H.{3,5}H";
 <>; # Skip first blank "line"
 
@@ -13,5 +14,6 @@ while(<>){
                                  # the first is the ID and the rest is the sequence
 	my $seq = join("", @seq); 
 	print ">$id\n$seq\n" 
-		if ($seq =~ s/($motif)/\-\[$1\]\-/gi);
+		if ($seq =~ s/($motif)/\-\[$1\]\-/gi); # the /g modifier means "match more than one time if possible"
+                                               # the /i modifier makes the match case insensitive
 }
