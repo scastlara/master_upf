11428704e2586ce1f3bb0681cd6ad8cec99bda1c
Sergio Castillo <s.cast.lara@gmail.com>
2015-10-21 21:42:09 +0200

    Comments

--- PERL/motif_finder.pl
+++ PERL/motif_finder.pl
@@ -1,13 +1,16 @@
 use warnings;
 use strict;
 
-local $/ = ">"; 
-<>; 
+local $/ = ">"; # This magic variable changes the record separator.
+                # It will make the diamond operator read from > to >
+                # instead of from \n to \n (line by line)
 my $motif = "C.{2,4}C.{3}[LIVMFYWC].{8}H.{3,5}H";
+<>; # Skip first blank "line"
 
 while(<>){
 	chomp; 
-	my ($id, @seq) = split /\n/; 
+	my ($id, @seq) = split /\n/; # split by lines, 
+                                 # the first is the ID and the rest is the sequence
 	my $seq = join("", @seq); 
 	print ">$id\n$seq\n" 
 		if ($seq =~ s/($motif)/\-\[$1\]\-/gi);
