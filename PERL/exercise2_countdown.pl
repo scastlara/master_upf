245b60f8f5c3147ede5c21bdde7d25f03f98d50a
durvasa <ferran.muinos@gmail.com>
2015-10-28 10:02:28 +0100

    Change print in exercise2_countdown.pl
    Not better than the previous one, just a different taste.

--- PERL/exercise2_countdown.pl
+++ PERL/exercise2_countdown.pl
@@ -4,7 +4,9 @@ use warnings;
 use strict;
 
 for (my $i = 10; $i > 0; $i--) {
-	print "$i\n";
+	print STDERR "$i \r";    # STDERR channel avoids the use of buffers,
+				 # so you can print out directly and avoid
+				 # sleeping issues.
 	sleep(1);
 }
 
