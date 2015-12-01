5965b91fcdbae1492de462d6a89537ee15b7488c
Sergio Castillo <s.cast.lara@gmail.com>
2015-12-01 12:18:49 +0100

    Updated.

--- PERL/bmarking.pl
+++ PERL/bmarking.pl
@@ -1,55 +1,44 @@
-#!/usr/bin/perl
-use warnings;
+#! perl -slw
 use strict;
-use Benchmark;
+use Benchmark qw[ cmpthese ];
 
-my $file = shift @ARGV;
+our $string = 'x'x5000;
 
-my $dot_data = _slurp($file);
-
- timethese(5,
-     {"substring" => "submethod",
-     "chop" => "chopmethod",
-     "regexp" => "regexpmethod"});
-
-#--------------------------------------------------------------------------------
-sub _slurp {
-    my $file   = shift;
-
-    local $/ = undef;
-
-    open my $fh, "<", $file;
-
-    my $dot = <$fh>;
-
-    $dot =~ s/\s+=\s+/=/g;
-    $dot =~ s/(\->|\-\-)/ $1 /g;
-    $dot =~ s/\n\s*#.*?\n/\n/g;
-
-    return $dot;
-}
-
-
-sub submethod {
-
-    for (my $i = 0; $i < length($dot_data); $i++) {
-        my $substring = substr($dot_data, $i, 1);
-    }
-
-    return;
-}
-
-
-sub chopmethod {
-    my $s = reverse($dot_data);
-    my $char;
-    while (length($s)) {
-        $char = chop($s);
-    }
-}
-
-sub regexpmethod {
-    
-    while ($dot_data =~ m/\G(.)/gsc) {
-    }
-}
+cmpthese -1, {
+    substr      => q[
+        for ( 0..length( $string)-1 ) {
+            my $c = substr $string, $_, 1;
+        }
+    ],
+    splitArray  => q[
+        my @c=split'',$string;
+        for( 0 .. $#c ){
+            my $c = $c[$_];
+        }
+    ],
+    splitFor    => q[
+        for( split'', $string ){
+            my $c = $_;
+        }
+    ],
+    unpack      => q[
+        for( unpack 'C*', $string ) {
+            my $c = chr;
+        }
+    ],
+    reverseChop => q[
+        my $s = reverse $string;
+        my $c;
+        $c = $_ while chop $s;
+    ],
+    chop        => q[
+        my $s = $string;
+        my $c;
+        $c = $_ while chop $s;
+    ],
+    ramfile        => q[
+        open my $ram, '<', \$string;
+        my $c;
+        $c = $_ while $_ = getc( $ram );
+    ],
+};
